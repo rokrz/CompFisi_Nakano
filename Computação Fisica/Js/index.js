20321a4cbc5e9ca3b5fe224ad81f1e275dc627e1
rokrz <41301000+rokrz@users.noreply.github.com>
2022-01-22 16:18:35 -0300

    Removed credentials and Added reference fields

--- Computação Fisica/Js/index.js
+++ Computação Fisica/Js/index.js
@@ -1,7 +1,7 @@
 const tmi = require('twitch-auth-tmi');
 const request = require('request-promise')
 const { StaticAuthProvider } = require('twitch-auth');
-const authProvider = new StaticAuthProvider('512c3u1tyf5fe22k03o4xboa5i4qgj', 'yqk4as93bilkymu1uy1l0d8wfd9haj');
+const authProvider = new StaticAuthProvider("clientID", "authProvider");
 var url = "";
 
 const movimentos = { "f": "forward", "frente": "forward", "direita": "right", "d": "right", "esquerda": "left", "e": "left", "tras": "backward", "t": "backward", "para": "stop", "p": "stop" }
@@ -18,7 +18,7 @@ const client = new tmi.Client({
         secure: true
     },
     authProvider: authProvider,
-    channels: ['pepita_BR']
+    channels: ['clientId']
 });
 client.connect().catch(console.error);
 client.on('message', (channel, tags, message, self) => {
